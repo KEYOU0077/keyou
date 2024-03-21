ea37887ee45d9ac70bbda931b69105c965a8d79f
KEYOU0077 <144232587+KEYOU0077@users.noreply.github.com>
2024-03-21 23:26:15 +0100

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+function joinIn() {
+    window.location.href = "signuppage.html";
+}
+
+function submitForm() {
+    // Show creation message
+    alert("Thank You For Joining The Reds . See You Soon GGMU");
+    
+}
